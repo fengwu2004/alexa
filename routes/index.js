ed1b1aa277bd725a8178da73e8a04dd18148a5f7
晏立 <yanli@yellfun.com>
2017-05-10 22:58:22 +0800

    save

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/findwordmean', function(req, res, next) {
+
+    res.send("ok, let us move!")
 });
 
 module.exports = router;
